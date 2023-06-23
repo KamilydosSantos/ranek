4a88a52a3d5e1f5defe55758871f63d843971f8e
kamily <kamily7803@gmail.com>
2023-06-23 10:55:48 -0300

    feat: search style

--- src/components/SearchProducts.vue
+++ src/components/SearchProducts.vue
@@ -1,8 +1,8 @@
 <template>
     <section>
-        <form action="">
-            <input name="search" id="search" type="text" v-model="search">
-            <input id="submit-search" type="submit" value="Buscar" @click.prevent="searchProducts">
+        <form class="search" action="">
+            <input name="search" class="search__text" id="search-text" type="text" placeholder="Buscar..." v-model="search">
+            <input id="search-btn" class="search__btn" type="submit" value="Buscar" @click.prevent="searchProducts">
         </form>
     </section>
 </template>
@@ -23,6 +23,31 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .search {
+    max-width: 600px;
+    margin: 30px auto 60px auto;
+    position: relative;
+    #search-text {
+      width: 100%;
+      padding: 20px;
+      border: none;
+      &:focus,
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+    #search-btn {
+      width: 62px;
+      height: 62px;
+      background: url("../assets/search.svg") no-repeat center center;
+      text-indent: -150px;
+      border: none;
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      box-shadow: none;
+      cursor: pointer;
+    }
+  }
 </style>
