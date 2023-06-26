5d2c28c7881dc20a8c2d5320ae8adef5df597cd7
kamily <kamily7803@gmail.com>
2023-06-26 10:58:22 -0300

    feat: add pagination range

--- src/components/PageProducts.vue
+++ src/components/PageProducts.vue
@@ -1,6 +1,8 @@
 <template>
     <ul class="pages" v-if="totalPages > 1">
-        <li class="pages__page" v-for="page in totalPages" :key="page"><router-link class="pages__link" :to="{query: query(page)}">{{ page }}</router-link></li>
+        <router-link :to="{query: query(1)}"> ❮ </router-link>
+        <li class="pages__page" v-for="page in pages" :key="page"><router-link class="pages__link" :to="{query: query(page)}">{{ page }}</router-link></li>
+        <router-link :to="{query: query(totalPages)}"> ❯ </router-link>
     </ul>
 </template>
 
@@ -25,6 +27,22 @@ export default{
         }
     },
     computed: {
+        pages() {
+            const current = Number(this.$route.query._page);
+            const range = 9;
+            const offset = Math.ceil(range/2);
+            const total = this.totalPages;
+            const pagesArray = [];
+
+            for (let i=1; i<=total; i++) {
+                pagesArray.push(i);
+            }
+
+            pagesArray.splice(0, current - offset);
+            pagesArray.splice(range, total);
+
+            return pagesArray;
+        },
         totalPages() {
             const total = this.totalProducts/ this.productsPerPage;
             return (total !== Infinity) ? Math.ceil(total) : 0;
@@ -35,10 +53,10 @@ export default{
 
 <style lang="scss">
 .pages {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     grid-column: 1/-1;
-    &__page {
-        display: inline-block;
-    }
     &__link {
         padding: 2px 8px;
         border-radius: 2px;
