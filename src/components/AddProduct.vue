19c04d76c291c5ceb1f657abc3a0b13f330d3db9
kamily <kamily7803@gmail.com>
2023-06-28 14:05:25 -0300

    feat: add new user products

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,15 +1,57 @@
 <template>
     <section>
-        <p>Adicionar Produto</p>
+        <form class="add-product">
+            <label for="name">Nome</label>
+            <input type="text" name="name" id="name" v-model="product.name">
+            <label for="price">Preço (R$)</label>
+            <input type="number" name="price" id="price" v-model="product.price">
+            <label for="pictures">Fotos</label>
+            <input type="file" name="pictures" id="pictures" ref="pictures">
+            <label for="description">Descrição</label>
+            <textarea name="description" id="description" v-model="product.description"></textarea>
+            <input class="btn" type="button" value="Adicionar Produto" @click.prevent="addProduct">
+        </form>
     </section>
 </template>
 
 <script>
+import { api } from '@/services.js';
+
 export default {
     name: "AddProduct",
+    data() {
+        return {
+            product: {
+                name: "",
+                price: "",
+                description: "",
+                pictures: null
+            }
+        }
+    },
+    methods: {
+        formattingProduct() {
+            this.product.user_id = this.$store.state.user.id;
+        },
+        addProduct() {
+            this.formattingProduct();
+            api.post("/products", this.product)
+            .then(() => {
+                this.$store.dispatch("getUserProducts");
+            })
+        }
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.add-product {
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    align-items: center;
+    margin-bottom: 60px;
+    .btn {
+        grid-column: 2;
+    }
+}
 </style>
