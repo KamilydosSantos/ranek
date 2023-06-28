75ee5f2f5e81cba92f89cfa627d08943c10e7a4d
kamily <kamily7803@gmail.com>
2023-06-28 09:56:01 -0300

    feat: user menu style

--- src/views/user/MyUser.vue
+++ src/views/user/MyUser.vue
@@ -15,7 +15,7 @@
                     <router-link class="user__element" :to="{name: 'edit'}">Editar Usuário</router-link>
                 </li>
                 <li>
-                    <button class="btn" @click.prevent="logout">Logout</button>
+                    <button class="user__element user__btn" @click="logout">Logout</button>
                 </li>
             </ul>
         </nav>
@@ -38,20 +38,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.user__list {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    grid-column: 1/-1;
+
+.user {
+    display: grid;
+    grid-template-columns: minmax(140px, 200px) 1fr;
+    max-width: 900px;
+    margin: 40px auto;
+    grid-gap: 30px;
+    padding: 20px;
+    &__element {
+        display: block;
+        background: #f4f7fc;
+        padding: 10px;
+        margin-bottom: 10px;
+        border-radius: 4px;
+        &.router-link-exact-active,
+        &:hover {
+            background: #87f;
+            color: #fff;
+        }
+    }
+    &__btn {
+        padding: 10px;
+        border: none;
+        width: 100%;
+        font-size: 1rem;
+        text-align: left;
+        font-family: Arial, Helvetica, sans-serif;
+        cursor: pointer;
+    }
 }
-.user__element {
-    padding: 2px 8px;
-    border-radius: 2px;
-    margin: 4px;
-    &.router-link-exact-active,
-    &:hover {
-        background: #87f;
-        color: #fff;
+
+@media screen and (max-width: 500px) {
+    .user {
+        grid-template-columns: 1fr;
+        margin: 0px auto;
     }
 }
 </style>
