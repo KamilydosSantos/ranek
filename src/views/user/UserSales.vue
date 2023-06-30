e1c960d82dbe898e29ee885c1fbf979064e6bc09
kamily <kamily7803@gmail.com>
2023-06-30 11:29:13 -0300

    feat: shopping and sale pages

--- src/views/user/UserSales.vue
+++ src/views/user/UserSales.vue
@@ -1,13 +1,84 @@
 <template>
-    <p>lista vendas</p>
+    <section>
+        <div class="sale" v-if="sale">
+            <h2 class="sale__title">Vendas</h2>
+            <div class="sale__wrapper" v-for="(buy, index) in sale" :key="index">
+                <ProductItem v-if="buy.product" :product="buy.product">
+                    <p class="sale__seller"><span>Comprador: </span>{{ buy.buyer_id }}</p>
+                </ProductItem>
+                <div class="sale__delivery">
+                    <h3 class="sale__delivery__title">Entrega: </h3>
+                    <ul v-if="buy.address">
+                        <li v-for="(value, key) in buy.address" :key="key">
+                            {{ key }}: {{ value }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-export default {
+import ProductItem from '@/components/ProductItem.vue';
+import { api } from "@/services.js"
+import { mapState } from 'vuex';
 
+export default {
+    name: "UserSales",
+    data() {
+        return {
+            sale: null,
+        };
+    },
+    components: { 
+        ProductItem 
+    },
+    computed: {
+        ...mapState(["user", "login"])
+    },
+    methods: {
+        getSale() {
+            api.get(`/transaction?seller_id=${this.user.id}`)
+            .then(response => {
+                this.sale = response.data;
+            })
+        }
+    },
+    watch: {
+        login() {
+            this.getSale();
+        }
+    },
+    created() {
+        if(this.login) {
+            this.getSale();
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+.sale {
+    &__title {
+        margin-bottom: 20px;
+    }
+    &__wrapper {
+        margin-bottom: 40px;
+    }
+    &__seller span{
+        color: #e80;
+    }
+    &__delivery {
+        display: grid;
+        grid-template-columns: minmax(100px, 200px) 1fr;
+        grid-gap: 20px;
+        margin-bottom: 60px;
+        &__title {
+            margin: 0;
+            justify-self: end;
+        }
+    }
+}
 </style>
--- src/views/user/UserShopping.vue
+++ src/views/user/UserShopping.vue
@@ -1,13 +1,66 @@
 <template>
-    <p>lista compras</p>
+    <section>
+        <div class="shopping" v-if="shopping">
+            <h2 class="shopping__title">Compras</h2>
+            <div class="shopping__wrapper" v-for="(buy, index) in shopping" :key="index">
+                <ProductItem v-if="buy.product" :product="buy.product">
+                    <p class="shopping__seller"><span>Vendedor: </span>{{ buy.seller_id }}</p>
+                </ProductItem>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-export default {
+import ProductItem from '@/components/ProductItem.vue';
+import { api } from "@/services.js"
+import { mapState } from 'vuex';
 
+export default {
+    name: "UserShopping",
+    data() {
+        return {
+            shopping: null,
+        };
+    },
+    components: { 
+        ProductItem 
+    },
+    computed: {
+        ...mapState(["user", "login"])
+    },
+    methods: {
+        getShopping() {
+            api.get(`/transaction?buyer_id=${this.user.id}`)
+            .then(response => {
+                this.shopping = response.data;
+            })
+        }
+    },
+    watch: {
+        login() {
+            this.getShopping();
+        }
+    },
+    created() {
+        if(this.login) {
+            this.getShopping();
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+.shopping {
+    .shopping__title {
+        margin-bottom: 20px;
+    }
+    .shopping__wrapper {
+        margin-bottom: 40px;
+    }
+    .shopping__seller span{
+        color: #e80;
+    }
+    }
 </style>
